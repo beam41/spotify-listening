80bb03ff2a25beebc7727a573b6ff84fa9219662
Meehoi <beam_baimon@hotmail.com>
2020-07-28 19:08:23 +0700

    forgot to build again

--- dist/index.js
+++ dist/index.js
@@ -70,10 +70,11 @@ const core = __webpack_require__(422);
 const fetch = __webpack_require__(795);
 
 async function main() {
-  const content = core.getInput("content");
+  let content = core.getInput("content");
   const token = core.getInput("token");
   const clientId = core.getInput("clientId");
   const cliSecret = core.getInput("cliSecret");
+  console.log("Getting access token..");
   const secret = `${clientId}:${cliSecret}`;
   const secretBase64 = Buffer.from(secret).toString("base64");
   const tokenRequest = await fetch("https://accounts.spotify.com/api/token", {
@@ -89,7 +90,8 @@ async function main() {
     }),
   });
   const tokenRequestData = await tokenRequest.json();
-
+  console.log("Access token received");
+  console.log("Fetch data");
   const res = await fetch(
     "https://api.spotify.com/v1/me/top/artists?time_range=short_term&limit=5",
     {
@@ -101,15 +103,23 @@ async function main() {
     }
   );
   const data = await res.json();
-  console.log(data)
-  const dataTable = `
+  console.log("Data fetched");
+  console.log("Write a table");
+  const dataTable = `<!-- table start -->
 |${data.items
     .map(({ images }) => `<img src="${images[images.length - 1].url}">`)
     .join("|")}|
 | :---: | :---: | :---: | :---: | :---: | :---: |
 |${data.items.map(({ name }) => `<b>${name}</b>`).join("|")}|
-  `.trim();
-  console.log(dataTable);
+
+Updated at ${new Date().toTimeString()}
+<!-- table end -->`;
+  console.log("Write new readme");
+  content = content.replace(
+    /<!-- *table start *-->[^]*<!-- *table end *-->/gi,
+    dataTable
+  );
+  core.setOutput("content", content);
 }
 
 main().catch((err) => core.setFailed(err.message));
